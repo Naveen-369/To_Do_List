94cc3bb891aba69062276e5d425eb618d2399870
Naveen-369 <nk349352@gmail.com>
2024-01-04 23:40:27 +0530

    Slight Modication

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Subcribe to Nk_Blogs
         </p>
         <a
           className="App-link"
