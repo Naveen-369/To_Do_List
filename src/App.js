dcb74fcb60b5c750d34d7bf07f706880bd077505
Naveen-369 <nk349352@gmail.com>
2024-01-06 09:29:07 +0530

    Learngin Functinal Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Header from './Header'
+//Importing the styles page
 import './App.css'
+//Importing the Components
+import Header from './Header'
 import Content from './Content';
+import Footer from './Footer';
+//Declaration of the Application
 function App() {
   return (
     <div className='Gun'>
         <Header />
         <Content />
+        <Footer />
     </div>
 );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    let obje=new Date();
+return (
+    <div className='CopyRight'>
+        <footer>CopyRight &copy; {obje.getFullYear()}</footer>
+    </div>
+)
+}
+
+export default Footer
