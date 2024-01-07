d6f5ebaa37ee88e9efc5c45a0ff78d3d54a8b7b6
Naveen-369 <nk349352@gmail.com>
2024-01-07 14:23:54 +0530

    ok

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,26 @@
 //Importing the CSS package
 import React from 'react';
+import { useState } from 'react';
 import './Content.css';
 //Declaration of the package
-const Content = () => {
+function Content() {
     function GreetUser(){
         const arr=["Naveen","Kumar","Liar"];
         let g = Math.floor(Math.random()*arr.length);
         return arr[g];
     }
+    //Declaration of the function that changes the values of the count
+    const[count,setCount]=useState(0);
+    function Increment(){setCount(count+1);}
+    function Decrement(){setCount(count-1);}
+    //Declaration of the JSX
     return (
     <main>
         <p>Hello {GreetUser()}</p>
+        <button type="button">Subscribe</button>
+        <button type="button" onClick={Decrement}>-</button>
+        <span>{count}</span>
+        <button type="button" onClick={Increment}>+</button>
     </main>
     )
 }
