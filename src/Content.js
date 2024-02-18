475bda0dc2dcfa5fa3181f303bc67f760c9bedd5
Naveen <nk9363690@gmail.com>
2024-02-18 11:01:18 +0530

    Trying to create a To-Do Application -> I am trying to use useState hooke and the react_icons for the application

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,46 @@
 //Importing the CSS package
-import React from 'react';
-//Declaration of the package
+import React, { useState } from 'react';
+import {FaTrashAlt} from 'react-icons/fa'; 
+
 function Content() {
-    //Declaration of the JSX
+    //Declaration of the Usestate hooks for the Seamless Creation and deletion experience
+    const [tasks]=useState([
+        {
+            id:1,
+            text:'Doctor Appointment',
+            day:'Feb 5th at 2:30pm',
+            reminder:true,
+        },
+        {
+            id:2,
+            text:'Meeting at School',
+            day:'Feb 6th at 1:30pm',
+            reminder:true,
+        },
+        {
+            id:3,
+            text:'Food Shopping',
+            day:'Feb 5th at 2:30pm',
+            reminder:false,
+        },
+    ])
+    //Returning the component
     return (
     <main>
+        <ul>
+            {
+                tasks.map((task)=>{
+                    return (<li key={task.id}>
+                        <input type="checkbox" checked={task.reminder}/>
+                        <label>{task.text}</label>
+                        <FaTrashAlt
+                            role='button'
+                            tabIndex={0}
+                        />
+                    </li>)
+                })
+            }
+        </ul>
     </main>
     )
 }
